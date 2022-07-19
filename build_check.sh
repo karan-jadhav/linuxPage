485e9edc8bb24a260a46da217c45fec91d1cf521
karan-jadhav <karan.jadhavhkr@gmail.com>
2022-07-19 13:06:50 +0530

    :skip: build_check.sh fixed with correct exit code

--- build_check.sh
+++ build_check.sh
@@ -6,8 +6,8 @@ commit_message=$(git log -1 --pretty=%B)
 # check if :skip: is in the commit message and if so, return with exit code 0 else return with exit code 1
 if [[ $commit_message == *":skip:"* ]]; then
   echo "Skipping build"
-#   exit 0
+  exit 0
 else
-    echo "Building"
-#   exit 1
+  echo "Building"
+  exit 1
 fi
